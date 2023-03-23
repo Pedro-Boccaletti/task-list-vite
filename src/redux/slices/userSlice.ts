1585469eeec455af9bb66bdbcc09912d471e0947
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-22 21:30:13 -0300

    adds redux to project

---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { User } from '../../typings/User'
+
+const initialState: {user: User} = {
+  user: {
+    id: '',
+    userName: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+  },
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    resetState: (state) => {
+      state = initialState;
+    }
+  }
+})
+
+export const { setUser, resetState } = userSlice.actions;
+
+export default userSlice.reducer;
