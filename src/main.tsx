303105dd52185652e8334ab7ac0243e2b1910e68
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-23 12:29:24 -0300

    adds tailwind globalstyles

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import GlobalStyles from './GlobalStyles'
-import './index.css'
 import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles />
     <Provider store={store}>
       <App />
     </Provider>
