2d80cfa16cc5f760c38e8e9c55e63c1b8a790c92
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-24 10:22:41 -0300

    feat: ListItem doesn't look like a placeholder anymore

--- src/containers/TaskList/ListItem.tsx
+++ src/containers/TaskList/ListItem.tsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import CheckBox from '../../components/CheckBox';
 import { Task } from '../../typings/Task'
+import { axiosReq } from '../../utils/axiosReq';
 import When from './When';
 
-const SItem = styled.li`
-  ${tw`flex justify-center items-center px-10`}
+const SItem = styled.div`
+  ${tw`flex justify-center items-center space-x-3 px-10`}
+`
 
-  ${tw`
+const STitle = styled.h5`
+ ${tw`
+    text-xl
+    md:text-2xl
+    text-black
+    font-medium
+    transition
+    duration-500
+    ease-in-out
+    hover:text-gray-600
+ `}
+`
+
+const SDescription = styled.p`
+ ${tw`
+    ml-2
     text-xs
     md:text-base
     text-black
-    font-medium
-    mr-1
-    md:mr-5
+    font-extralight
     transition
     duration-500
     ease-in-out
     hover:text-gray-700
-  `}
-
-  border: 1px solid red;
+ `}
 `
 
 type Props = {
@@ -28,11 +42,25 @@ type Props = {
 }
 
 function ListItem({ task }: Props) {
+  const checkRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (checkRef.current)
+      checkRef.current.checked = task.complete
+  }, [checkRef.current])
+
   return (
     <SItem>
-      <div style={{border: 'solid 1px black'}}>{task.title}</div>
-      <div style={{border: 'solid 1px green'}}>{task.description}</div>
-      <When date={task.date}/>
+      <CheckBox forwardRef={checkRef} onChange={(e) => {
+        axiosReq().patch(`task/${task.id}`, {
+          complete: e.target.checked,
+        })
+      }} />
+      <div>
+        <STitle>{task.title}</STitle>
+        {task.description && <SDescription>{task.description}</SDescription> }
+        <When date={task.date}/>
+      </div>
       
     </SItem>
   )
