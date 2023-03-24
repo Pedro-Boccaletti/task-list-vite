dfc984422c82521c23d51b13975f9aea97be33d2
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-24 20:26:41 -0300

    feat: adds reactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 import App from './App'
 import GlobalStyles from './GlobalStyles'
 import { store } from './redux/store'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyles />
-    <MemoryRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </MemoryRouter>
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </MemoryRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
