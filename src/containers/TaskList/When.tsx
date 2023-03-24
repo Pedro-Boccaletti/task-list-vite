3a77f8e7ec30b535a234b4a4ba2d96949bbf3ade
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-24 08:01:11 -0300

    feat: properly shows When

--- src/containers/TaskList/When.tsx
+++ src/containers/TaskList/When.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
-// import { Date } from '../typings/Item'
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 type Props = {
   date?: Date;
 }
 
+const DateContainer = styled.div`
+  ${tw`inline-flex justify-center rounded-3xl px-3 py-1 bg-gray-200`}
+`
+
+const getMonthString = (date: Date): [string, string] => {
+  const monthLong = date.toLocaleDateString(undefined, { month: 'long' })
+  const monthShort = date.toLocaleDateString(undefined, { month: 'short' })
+  return [monthLong,monthShort]
+}
+
+const getWeekDay = (date: Date): string => {
+  const weekDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
+  return weekDays[date.getDay()]
+}
+
 function When({ date }: Props) {
+  if (!date) return
+  const day = date.toLocaleDateString(undefined, { day: '2-digit' })
+  const month = getMonthString(date)
+  const year = date.getFullYear()
+  const weekDay = getWeekDay(date)
+  if (new Date().getFullYear() !== year) return (
+    <DateContainer>{`${day} de ${month[0]} de ${year}`}</DateContainer>
+  )
+
   return (
-    <div>{date?.toUTCString()}</div>
+    <DateContainer>{`${weekDay}, ${day} de ${month[1]}`}</DateContainer>
   )
 }
 
