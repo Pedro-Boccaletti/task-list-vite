3eafea32a25b70520df9fb9f13099529f26bc53e
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-23 23:03:43 -0300

    feat: adds router to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router-dom'
 import App from './App'
 import GlobalStyles from './GlobalStyles'
 import { store } from './redux/store'
@@ -8,8 +9,10 @@ import { store } from './redux/store'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyles />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
   </React.StrictMode>,
 )
