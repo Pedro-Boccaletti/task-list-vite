32027cdb76f86f1be15e8f3a808ff1dd85637116
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-24 10:19:24 -0300

    feat: adds checkbox component

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,59 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+type CheckProps = {
+  name?: string
+  className?: string
+  forwardRef?: React.RefObject<HTMLInputElement>
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  checked?: boolean
+}
+
+
+const SLabel = styled.label`
+  ${tw`flex border-2 rounded-full justify-center items-center p-1 w-fit`}
+`;
+
+const SCheckBox = styled.input`
+  ${tw`
+    h-3
+    w-3
+    outline-0
+    appearance-none
+    border-2
+    rounded-full
+    border-gray-500
+    cursor-pointer
+    transition
+    duration-500
+    ease-in-out
+  `}
+
+  &:hover {
+    ${tw`bg-gray-300`}
+  }
+
+  &:checked {
+    ${tw`bg-blue-700 border-0`}
+  }
+`
+
+function CheckBox({ name, className, forwardRef, onChange, checked }:CheckProps) {
+  if (!name) return (
+    <SCheckBox type='checkbox' ref={forwardRef} onChange={onChange} checked={checked} />
+  )
+  return (
+    <SLabel className={className}>
+      <SCheckBox
+        type='checkbox'
+        ref={forwardRef}
+        onChange={onChange}
+        checked={checked}
+      />
+      {name}
+    </SLabel>
+  );
+}
+
+export default CheckBox;
