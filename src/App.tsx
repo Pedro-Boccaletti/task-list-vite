848b926a8961cba15345126a9508bcaaf434d051
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-23 23:05:39 -0300

    feat: renders router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import List from './containers/TaskList/List';
+import LoginForm from './containers/Login/LoginForm';
+import { Route, Routes } from 'react-router-dom';
+
+const AppContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+  `}
+  height: 100vh;
+  width: 100vw;
+`;
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <AppContainer>
+      <Routes>
+        <Route path='/' element={<LoginForm />} />
+        <Route path='/tasks' element={<List />} />
+      </Routes>
+    </AppContainer>
   )
 }
 
-export default App
+export default App;
